fd653158977061539ac561afea8e992f898e7a4a
Stoo <stoo.goff@gmail.comn>
2022-12-22 15:54:30 +0000

    Don't log empty messages

--- server/errors.js
+++ server/errors.js
@@ -7,19 +7,25 @@ const error = (status, message) => ({
 })
 
 export const badRequest = (message = '') => {
-	logger.info(message)
+	if(message) {
+		logger.info(message)
+	}
 
 	return error(400, `Bad Request. ${message}`)
 }
 
 export const notFound = (message = '') => {
-	logger.info(message)
+	if(message) {
+		logger.info(message)
+	}
 
 	return error(404, `Not found. ${message}`)
 }
 
 export const serverError = (message = '') => {
-	logger.error(message)
+	if(message) {
+		logger.error(message)
+	}
 
 	return error(500, `Internal Server Error. ${message}`)
 }
