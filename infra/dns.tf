3c9899fb99ce8e238700d48732950764030b7485
Stoo <stoo.goff@gmail.com>
2025-05-14 17:33:30 +0100

    Added bsky domain verification

--- infra/dns.tf
+++ infra/dns.tf
@@ -4,32 +4,28 @@ data "bunnynet_dns_zone" "dns" {
 }
 
 resource "bunnynet_dns_record" "bare" {
-	zone = data.bunnynet_dns_zone.dns.id
-
+	zone  = data.bunnynet_dns_zone.dns.id
 	name  = ""
 	type  = "CNAME"
 	value = "${bunnynet_pullzone.www.name}.${bunnynet_pullzone.www.cdn_domain}"
 }
 
 resource "bunnynet_dns_record" "www" {
-	zone = data.bunnynet_dns_zone.dns.id
-
+	zone  = data.bunnynet_dns_zone.dns.id
 	name  = "www"
 	type  = "CNAME"
 	value = "${bunnynet_pullzone.www.name}.${bunnynet_pullzone.www.cdn_domain}"
 }
 
 resource "bunnynet_dns_record" "cdn" {
-	zone = data.bunnynet_dns_zone.dns.id
-
+	zone  = data.bunnynet_dns_zone.dns.id
 	name  = "cdn"
 	type  = "CNAME"
 	value = "${bunnynet_pullzone.cdn.name}.${bunnynet_pullzone.cdn.cdn_domain}"
 }
 
 resource "bunnynet_dns_record" "games" {
-	zone = data.bunnynet_dns_zone.dns.id
-
+	zone  = data.bunnynet_dns_zone.dns.id
 	name  = "games"
 	type  = "CNAME"
 	value = "${bunnynet_pullzone.games.name}.${bunnynet_pullzone.games.cdn_domain}"
@@ -37,18 +33,24 @@ resource "bunnynet_dns_record" "games" {
 
 
 resource "bunnynet_dns_record" "mail" {
-	zone = data.bunnynet_dns_zone.dns.id
-
+	zone  = data.bunnynet_dns_zone.dns.id
 	name  = "mail"
 	type  = "A"
 	value = var.mail_server
 }
 
 resource "bunnynet_dns_record" "mx" {
-	zone = data.bunnynet_dns_zone.dns.id
-
+	zone     = data.bunnynet_dns_zone.dns.id
 	name     = ""
 	type     = "MX"
 	value    = "${bunnynet_dns_record.mail.name}.${data.bunnynet_dns_zone.dns.domain}"
 	priority = 10
+}
+
+resource "bunnynet_dns_record" "atprotocol" {
+	zone    = data.bunnynet_dns_zone.dns.id
+	name    = "_atproto"
+	type    = "TXT"
+	value   = "did=did:plc:aqppmthj2zyz6j3wrx76vbyr"
+	comment = "bsky atprotocol verification"
 }
